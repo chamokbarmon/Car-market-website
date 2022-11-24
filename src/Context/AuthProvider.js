c6788a280ac6869c95ff66125b059a43f4df1184
chamokBarmon <chamokbarmon123@gmail.com>
2022-11-24 19:45:17 +0600

    five-commit

--- src/Context/AuthProvider.js
+++ src/Context/AuthProvider.js
@@ -7,14 +7,18 @@ const auth =getAuth(app)
 
 const AuthProvider = ({children}) => {
     const [user,setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
    
     const createUser = (email,password)=>{
+        setLoading(true)
         return createUserWithEmailAndPassword(auth,email,password)
     }
     const signIn =(email,password)=>{
+        setLoading(true);
         return signInWithEmailAndPassword(auth,email,password);
     }
     const logOut =()=>{
+        setLoading(true);
         return signOut(auth)
     }
     const updateUser = (userinfo)=>{
@@ -25,6 +29,7 @@ const AuthProvider = ({children}) => {
       const unsubscribe = onAuthStateChanged(auth,currentUser=>{
         console.log('user observing')
         setUser(currentUser)
+        setLoading(false)
       })
       return ()=> unsubscribe();
 
@@ -35,6 +40,7 @@ const AuthProvider = ({children}) => {
        createUser,
        signIn,
        updateUser,
+       loading,
        logOut
     }
     return (
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,17 +1,22 @@
 import { useContext } from 'react';
 import { useForm } from 'react-hook-form';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../Context/AuthProvider';
 
 const Login = () => {
     const {signIn} = useContext(AuthContext)
     const { register, handleSubmit } = useForm();
+    const location = useLocation()
+    const navigate = useNavigate()
+
+    const from = location.state?.from?.pathname || '/'
     const handellogin = data =>{
         console.log(data)
         signIn(data.email,data.password)
         .then(result=>{
             const user = result.user;
             console.log(user)
+            navigate(from,{replace:true})
         })
         .catch(error=>console.log(error))
 
